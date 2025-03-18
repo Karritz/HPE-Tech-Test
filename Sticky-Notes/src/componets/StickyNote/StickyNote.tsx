635ff191376cde4ea2cc1954fad75895fac8a50d
Gareth Williams <65289470+Karritz@users.noreply.github.com>
2025-03-18 16:49:25 +0200

    styling changes

--- Sticky-Notes/src/componets/StickyNote/StickyNote.tsx
+++ Sticky-Notes/src/componets/StickyNote/StickyNote.tsx
@@ -1,11 +1,20 @@
 import './StickyNote.css'
 
-function StickyNote(prop: { x: number, y: number, teamMember: {name: string, color: string}, task: string, isComplete: boolean}) {
+function StickyNote(note: { x: number, y: number, teamMember: { name: string, color: string }, task: string, isComplete: boolean }) {
     return (
         <>
-            <div className="note" style={{backgroundColor: prop.teamMember.color, position: 'absolute', top: prop.y, left: prop.x}}>
+            <div className="note" style={{ backgroundColor: note.teamMember.color, position: 'absolute', top: note.y, left: note.x }}>
                 <div>
-                    Assignee: {prop.teamMember.name}
+                    Assignee: {note.teamMember.name}
+                </div>
+                <div>
+                    Prioraty:
+                </div>
+                <div>
+                    Task:
+                </div>
+                <div>
+                    Complete:
                 </div>
             </div>
         </>
